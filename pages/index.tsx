4ea15217e1554307890a0ca4cdc35c463bd5967b
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-02-27 09:30:04 +0100

    refactor: drop not relevant content

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,6 @@ export default function Home() {
           name="description"
           content="Your new way to find connections between trains in Switzerland"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello in Departurely</h1>
     </>
