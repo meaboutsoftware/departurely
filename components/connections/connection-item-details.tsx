5e35418f575e9157d793f68871d365775670a22f
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-02-28 20:37:33 +0100

    feat: add styling to connection item details

--- components/connections/connection-item-details.tsx
+++ components/connections/connection-item-details.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import uuid from "react-uuid";
 import { ConnectionStopover } from "./connection-stopover";
-import { formatTime } from "./time-formatter";
 
 type ConnectionDetails = {
   arrivalTime: string;
@@ -9,7 +8,6 @@ type ConnectionDetails = {
   from: string;
   sections: ConnectionStopover[];
   to: string;
-  transfers: number;
 };
 
 export default function ConnectionItemDetails({
@@ -18,7 +16,6 @@ export default function ConnectionItemDetails({
   from,
   sections,
   to,
-  transfers,
 }: ConnectionDetails) {
   return (
     <div data-testid="item-details">
@@ -30,25 +27,55 @@ export default function ConnectionItemDetails({
       <div data-testid="item-details-arrival-time">
         Arrival time: {arrivalTime}
       </div>
-      <div data-testid="item-details-stopovers">
-        Number of stopovers: {transfers}
-      </div>
-      <ul>
+      <table className="border-collapse w-full mt-4">
+        <thead>
+          <tr>
+            <th className="btext-left py-2 px-4 bg-gray-100 font-bold uppercase text-sm text-gray-600 border-b border-gray-200">
+              Transport number
+            </th>
+            <th className="btext-left py-2 px-4 bg-gray-100 font-bold uppercase text-sm text-gray-600 border-b border-gray-200">
+              Departure
+            </th>
+            <th className="btext-left py-2 px-4 bg-gray-100 font-bold uppercase text-sm text-gray-600 border-b border-gray-200">
+              Track
+            </th>
+            <th className="btext-left py-2 px-4 bg-gray-100 font-bold uppercase text-sm text-gray-600 border-b border-gray-200">
+              Arrival
+            </th>
+            <th className="btext-left py-2 px-4 bg-gray-100 font-bold uppercase text-sm text-gray-600 border-b border-gray-200">
+              Track
+            </th>
+          </tr>
+        </thead>
         {sections.map((section) => {
           return (
             section.journey && (
-              <li key={uuid()}>
-                {section.journey.category} {section.journey.number} - Departure:{" "}
-                {section.departure.station.name}, Track:{" "}
-                {section.departure.platform}{" "}
-                {formatTime(section.departure.departure)} Arrival:{" "}
-                {section.arrival.station.name}, Track:{" "}
-                {section.arrival.platform} {formatTime(section.arrival.arrival)}
-              </li>
+              <tbody>
+                <tr
+                  key={uuid()}
+                  className="border-b border-gray-200 hover:bg-gray-100"
+                >
+                  <td className="py-2 px-4 text-center bg-gray-200">
+                    {section.journey.category} {section.journey.number}
+                  </td>
+                  <td className="py-2 px-4 text-center bg-gray-200">
+                    {section.departure.station.name}
+                  </td>
+                  <td className="py-2 px-4 text-center bg-gray-200">
+                    {section.departure.platform}
+                  </td>
+                  <td className="py-2 px-4 text-center bg-gray-200">
+                    {section.arrival.station.name}
+                  </td>
+                  <td className="py-2 px-4 text-center bg-gray-200">
+                    {section.arrival.platform}
+                  </td>
+                </tr>
+              </tbody>
             )
           );
         })}
-      </ul>
+      </table>
       <br />
     </div>
   );
