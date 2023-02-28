478df9ee4081275b87fb72760d0b68038c605f1f
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-02-28 17:52:44 +0100

    refactor: change way of router mocking

--- components/home/search-form.test.tsx
+++ components/home/search-form.test.tsx
@@ -3,8 +3,9 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import SearchForm from "./search-form";
 
-// eslint-disable-next-line global-require, import/no-extraneous-dependencies, unicorn/prefer-module
-jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
 
 describe("SearchForm", () => {
   it("renders all DOM elements when all values are defined", () => {
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -3,8 +3,9 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from ".";
 
-// eslint-disable-next-line global-require, import/no-extraneous-dependencies, unicorn/prefer-module
-jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
 
 describe("Home", () => {
   it("renders child component", () => {
