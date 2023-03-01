f7e53af902cf0cea2a5791c24f1e1d047710bb83
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-03-01 12:03:46 +0100

    fix: styling of time and connector

--- components/connections/connection-item-overview.tsx
+++ components/connections/connection-item-overview.tsx
@@ -17,7 +17,7 @@ export default function ConnectionItemOverview({
     <div data-testid="item-overview">
       <div className="flex flex-row">
         <div
-          className="basis-1/4 text-2xl text-center"
+          className="basis-1/4 text-2xl text-right"
           data-testid="item-overview-departure-time"
         >
           {departureTime}
@@ -26,21 +26,21 @@ export default function ConnectionItemOverview({
           className="basis-1/4 text-center"
           data-testid="item-overview-stopovers"
         >
-          <svg height="20" width="100%">
-            <circle cx="5" cy="15" r="5" fill="black" />
-            <line x1="0" y1="15" x2="98%" y2="15" stroke="black" />
-            <circle cx="98%" cy="15" r="5" fill="black" />
+          <svg height="20" width="100%" viewBox="0 0 100 20">
+            <circle cx="5" cy="10" r="5" fill="black" />
+            <line x1="0" y1="10" x2="100" y2="10" stroke="black" />
+            <circle cx="95" cy="10" r="5" fill="black" />
           </svg>
           Stops: {transfers}
         </div>
         <div
-          className="basis-1/4 text-2xl text-center"
+          className="basis-1/4 text-2xl text-left"
           data-testid="item-overview-arrival-time"
         >
           {arrivalTime}
         </div>
         <div
-          className="basis-1/4 text-center"
+          className="basis-1/4 text-left"
           data-testid="item-overview-duration"
         >
           Duration: {duration}
